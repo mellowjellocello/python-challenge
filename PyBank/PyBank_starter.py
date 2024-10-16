b7b61a61333ac0575ff22b6d142397ea0bc47111
mellowjellocello <sarah.pieceofcheese@gmail.com>
2024-10-16 11:09:06 -0600

    first submission

--- PyBank/PyBank_starter.py
+++ PyBank/PyBank_starter.py
@@ -2,17 +2,24 @@
 """PyBank Homework Starter File."""
 
 # Dependencies
-import csv
 import os
+import csv
+
+from dask.array import average
+from openpyxl.styles.builtins import output, total
 
 # Files to load and output (update with correct file paths)
 file_to_load = os.path.join("Resources", "budget_data.csv")  # Input file path
 file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
 
 # Define variables to track the financial data
-total_months = 0
+total_months = 1
 total_net = 0
+
 # Add more variables to track other necessary financial data
+changes = []
+gincrease = 0
+gdecrease = 0
 
 # Open and read the csv
 with open(file_to_load) as financial_data:
@@ -22,35 +29,51 @@ with open(file_to_load) as financial_data:
     header = next(reader)
 
     # Extract first row to avoid appending to net_change_list
-
+    row1 = next(reader)
 
     # Track the total and net change
-
+    total_net += int(row1[1])
+    previncome = int(row1[1])
 
     # Process each row of data
     for row in reader:
+        income = int(row[1])
 
         # Track the total
-
+        total_months += 1
+        total_net += income
 
         # Track the net change
-
+        change = income - previncome
+        changes.append(change)
+        previncome = income
 
         # Calculate the greatest increase in profits (month and amount)
-
+        if change > gincrease:
+            gincrease = change
+            gincreasem = row[0]
+        elif change < gdecrease:
+            gdecrease = change
+            gdecreasem = row[0]
 
         # Calculate the greatest decrease in losses (month and amount)
 
 
-
 # Calculate the average net change across the months
-
+avgchange = sum(changes)/len(changes)
 
 # Generate the output summary
-
+output = f"""
+Financial Analysis
+----------------------------
+Total Months: {total_months}
+Total: ${total_net}
+Average Change: ${round(avgchange, 2)}
+Greatest Increase in Profits: {gincreasem} (${gincrease})
+Greatest Decrease in Profits: {gdecreasem} (${gdecrease})"""
 
 # Print the output
-
+print(output)
 
 # Write the results to a text file
 with open(file_to_output, "w") as txt_file:
--- PyPoll/PyPoll_starter.py
+++ PyPoll/PyPoll_starter.py
@@ -2,8 +2,14 @@
 """PyPoll Homework Starter File."""
 
 # Import necessary modules
-import csv
 import os
+import csv
+
+from PIL.ImImagePlugin import split
+from mypyc.namegen import candidate_suffixes
+from numpy.ma.extras import row_stack
+from spyder.utils.external.lockfile import unique
+from sympy.codegen.cnodes import union
 
 # Files to load and output (update with correct file paths)
 file_to_load = os.path.join("Resources", "election_data.csv")  # Input file path
@@ -13,10 +19,11 @@ file_to_output = os.path.join("analysis", "election_analysis.txt")  # Output fil
 total_votes = 0  # Track the total number of votes cast
 
 # Define lists and dictionaries to track candidate names and vote counts
-
+candidates = {}
+candidateinfolist = []
+highestvotes = 0
 
 # Winning Candidate and Winning Count Tracker
-
 
 # Open the CSV file and process it
 with open(file_to_load) as election_data:
@@ -29,42 +36,45 @@ with open(file_to_load) as election_data:
     for row in reader:
 
         # Print a loading indicator (for large datasets)
-        print(". ", end="")
+        # print(". ", end="") excluded this because i thought it was unnecessary and annoying lol
 
         # Increment the total vote count for each row
-
+        total_votes += 1
 
         # Get the candidate's name from the row
-
+        candidate = row[2]
 
         # If the candidate is not already in the candidate list, add them
-
+        if candidate not in candidates:
+            candidates[f'{candidate}'] = 0
 
         # Add a vote to the candidate's count
+        candidates[f'{candidate}'] += 1
 
+# determining results and formatting
+for c in candidates:
+    candidateinfo = f"{c}: {round(candidates[c]/total_votes*100,3)}% ({candidates[c]})"
+    candidateinfolist.append(candidateinfo)
+
+    if candidates[c] > highestvotes:
+        highestvotes = candidates[c]
+        currentwinner = c
+
+candidateresults = "\n".join(row for row in candidateinfolist)
+
+results = f"""
+Election Results
+-------------------------
+Total Votes: {total_votes}
+-------------------------
+{candidateresults}
+-------------------------
+Winner: {currentwinner}
+-------------------------"""
+
+print(results)
 
 # Open a text file to save the output
 with open(file_to_output, "w") as txt_file:
+    txt_file.write(results)
 
-    # Print the total vote count (to terminal)
-
-
-    # Write the total vote count to the text file
-
-
-    # Loop through the candidates to determine vote percentages and identify the winner
-
-
-        # Get the vote count and calculate the percentage
-
-
-        # Update the winning candidate if this one has more votes
-
-
-        # Print and save each candidate's vote count and percentage
-
-
-    # Generate and print the winning candidate summary
-
-
-    # Save the winning candidate summary to the text file
